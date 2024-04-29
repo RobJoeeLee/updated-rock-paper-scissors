5f4dab1c8b016dbb086718b4e414f276fe795e9b
Rob White <155823032+RobJoeeLee@users.noreply.github.com>
2024-04-29 08:21:36 -0400

    writing the logic to get computer choice and human choice

---
+++ index.js
@@ -0,0 +1,19 @@
+//Write the logic to get the comuter choice.
+    //Create a new function named getComputerChoice.
+    //Write the code so that getComputerChoice will randomly return one of the following string values: "rock", "paper", or "scissors".
+function getComputerChoice(){
+    const choices = ["rock", "paper", "scissors"];
+    const randomNum = Math.floor(Math.random() * 3);
+    return choices[randomNum];
+}
+
+//Write the logic to get the human choice.
+    //Create a new function named getHumanChoice.
+    //Write the code so that getHumanChoice will return one of the valid choices depending on what the user inputs.
+function getHumanChoice(){
+    let userInput = prompt("Enter your choice (rock, paper, or scissors):").toLowerCase();
+    while (userInput !== "rock" && userInput !== "paper" && userInput !== "scissors"){
+        userInput = prompt("Invalid choice! Please enter rock, paper, or scissors:").toLowerCase();
+    }
+    return userInput;
+}
